a260c87eb4eb96eebed70cbcb6ba0b88ea34e95b
Meitar Byle <maytarb@gmail.com>
2016-06-21 17:48:10 +0300

    Added draw panel and color picker, draw panels still not working

---
+++ Hamburger.UI/Views/ColorPicker.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Hamburger.UI.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ColorPicker : UserControl
+    {
+        public Colors SelectedColor { get; set; }
+
+        public delegate void ColorChangedEventHandler(object sender, Brush e);
+
+        public event ColorChangedEventHandler ColorChanged;
+
+        public ColorPicker()
+        {
+            this.InitializeComponent();
+            loadcolors();
+        }
+
+        public void loadcolors()
+        {
+            List<ColorInfo> myColors = new List<ColorInfo>();
+            myColors.Add(new ColorInfo("Red", Colors.Red));
+            myColors.Add(new ColorInfo("Blue", Colors.Blue));
+            myColors.Add(new ColorInfo("Yellow", Colors.Yellow));
+            myColors.Add(new ColorInfo("Green", Colors.Green));
+            myColors.Add(new ColorInfo("Orange", Colors.Orange));
+            myColors.Add(new ColorInfo("Purple", Colors.Purple));
+            colorList.ItemsSource = myColors;
+        }
+
+        private void colorList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ColorChanged != null)
+            {
+                ColorChanged(this, (((ColorInfo)e.AddedItems[0]).ColorBrush));
+            }
+        }
+    }
+    public class ColorInfo
+    {
+        public ColorInfo(string ColorName, Color Color)
+        {
+            this.Color = ColorName;
+            this.ColorBrush = new SolidColorBrush(Color);
+
+        }
+        public string Color
+        {
+            get;
+            set;
+        }
+        public Brush ColorBrush
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Hamburger.UI/Views/PaintToolBox.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Hamburger.UI.Views
+{
+    public sealed partial class PaintToolBox : UserControl
+    {
+        public Brush SelectedColor { get; set; } = new SolidColorBrush(Colors.Black);
+        public PaintToolBox()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ColorPickButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ColorPicker.Visibility == Visibility.Visible)
+            {
+                ColorPicker.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ColorPicker.Visibility = Visibility.Visible;
+                ColorPicker.Focus(FocusState.Programmatic);
+            }
+        }
+
+        private void ColorPicker_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ColorPicker.Visibility = Visibility.Collapsed;
+        }
+
+        private void ColorPicker_ColorChanged(object sender, Brush e)
+        {
+            SelectedColor = e;
+            ColorPickButton.Foreground = e;
+        }
+
+        private void FreeHandButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
