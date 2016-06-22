0d5a82e5fd64d5a59686544e350a3f037854de74
27182818 <leibowcal@gmail.com>
2016-06-22 18:06:09 +0300

    Add files via upload
    Works well!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,85 +16,33 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace HelloWorld
+namespace PartyViewer
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        DispatcherTimer timer = new DispatcherTimer();
         public MainPage()
         {
             this.InitializeComponent();
+            List<Image> partyImages = new List<Image>();
+            Image image1 = new Image();
+            image1.Source = new BitmapImage(new Uri("ms-appx:///Assets/small.bmp"));
+            Image image2 = new Image();
+            image2.Source = new BitmapImage(new Uri("ms-appx:///Assets/medium.bmp"));
+            Image image3 = new Image();
+            image3.Source = new BitmapImage(new Uri("ms-appx:///Assets/large.bmp"));
+            Image image4 = new Image();
+            image4.Source = new BitmapImage(new Uri("ms-appx:///Assets/list.bmp"));
+            partyImages.Add(image1);
+            partyImages.Add(image2);
+            partyImages.Add(image3);
+            partyImages.Add(image4);
+            PartyDisplay partyDisplay = new PartyDisplay();
+            this.Content = partyDisplay;
+            partyDisplay.Images = partyImages;
 
-            foreach(object scrollviewer in flipView.Items.ToList())
-            {
-                Image pic = new Image();
-                pic.Source = ((Image)((ScrollViewer)scrollviewer).Content).Source;
-                pic.Opacity = 0.3;
-                locationBar.Children.Add(pic);
-            }
-
-            ScrollViewer firstScroll = (ScrollViewer)flipView.Items.First();
-            ScrollViewer lastScroll = (ScrollViewer)flipView.Items.Last();
-            Image firstPic = new Image();
-            Image lastPic = new Image();
-            firstPic.Source = ((Image)firstScroll.Content).Source;
-            lastPic.Source = ((Image)lastScroll.Content).Source;
-            ScrollViewer scrvw1 = new ScrollViewer();
-            ScrollViewer scrvw2 = new ScrollViewer();
-            scrvw1.Content = firstPic;
-            scrvw2.Content = lastPic;
-            scrvw1.ZoomMode = ZoomMode.Enabled;
-            scrvw2.ZoomMode = ZoomMode.Enabled;
-            flipView.Items.Add(scrvw1);
-            List<object> items = flipView.Items.ToList();
-            items.Insert(0, scrvw2);
-            flipView.ItemsSource = items;
-            flipView.SelectedIndex = 1;
-            Debug.WriteLine(flipView.Items.ToString());
         }
-
-        private void flipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            timer.Stop();
-            locationBar.Visibility = Visibility.Visible;
-            timer.Tick += CollapseLocationBar;
-            timer.Interval = new TimeSpan(0, 0, 3);
-            timer.Start();
-            int index = ((FlipView)sender).SelectedIndex;
-            if (index == 0)
-            {
-                ((FlipView)sender).SelectedIndex = ((FlipView)sender).Items.Count - 2;
-            }
-            if (index == ((FlipView)sender).Items.Count - 1)
-            {
-                ((FlipView)sender).SelectedIndex = 1;
-            }
-            if(locationBar.Children.Count >= index && index > 0)
-            {
-                foreach(Image pic in locationBar.Children)
-                {
-                    pic.Opacity = 0.3;
-                }
-                locationBar.Children.ElementAt(index - 1).Opacity = 1;
-            }
-            if(index == 0 || index == 2)
-            {
-                ((ScrollViewer)flipView.Items.ElementAt(flipView.Items.Count - 1)).ZoomToFactor(((ScrollViewer)flipView.Items.ElementAt(1)).ZoomFactor);
-            }
-            if (index == flipView.Items.Count - 2 || index == 0)
-            {
-                ((ScrollViewer)flipView.Items.ElementAt(0)).ZoomToFactor(((ScrollViewer)flipView.Items.ElementAt(flipView.Items.Count - 2)).ZoomFactor);
-            }
-            //UpdateLayout();
-        }
-
-        void CollapseLocationBar(object sender, object e)
-        {
-            locationBar.Visibility = Visibility.Collapsed;
-        }
-
     }
 }
---
+++ PartyDisplay.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// Displays pictures of parties!
+
+namespace PartyViewer
+{
+    public sealed partial class PartyDisplay : UserControl
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+        public int timeToDisplayLocationBar = 3;
+        public double transparency = 0.3;
+        public bool allowClickOnLocationBar = true;
+
+        private List<Image> _Images;
+        public List<Image> Images {
+            get { return _Images; }
+            set {
+                _Images = value;
+                LoadImages();
+                currentIndex = 1;
+            }
+        }
+
+        public int currentIndex
+        {
+            get
+            {
+                return flipView.SelectedIndex;
+            }
+            set
+            {
+                flipView.SelectedIndex = value;
+            }
+        }
+
+        public PartyDisplay()
+        {
+            this.InitializeComponent();
+            timer.Tick += CollapseLocationBar;
+            timer.Interval = new TimeSpan(0, 0, timeToDisplayLocationBar);
+        }
+
+        internal void LoadImages()
+        {
+            Image firstImage = new Image();
+            Image lastImage = new Image();
+            firstImage.Source = Images.Last().Source;
+            lastImage.Source = Images.First().Source;
+            flipView.Items.Add(ConstructScrollViewer(firstImage));
+            foreach( Image image in Images)
+            {
+                flipView.Items.Add(ConstructScrollViewer(image));
+                Image previewPic = new Image();
+                previewPic.Source = image.Source;
+                previewPic.Opacity = transparency;
+                previewPic.PointerPressed += NavigateToPic;
+                locationBar.Children.Add(previewPic);
+            }
+            flipView.Items.Add(ConstructScrollViewer(lastImage));           
+        }
+
+        internal ScrollViewer ConstructScrollViewer(Image image)
+        {
+            ScrollViewer scrollViewer = new ScrollViewer();
+            scrollViewer.ZoomMode = ZoomMode.Enabled;
+            scrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            scrollViewer.Content = image;
+            return scrollViewer;
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ResetTimer();
+            ManageLocationBarSelected();
+            ManageCyclicalNavigation();
+        }
+
+        void CollapseLocationBar(object sender, object e)
+        {
+            locationBar.Visibility = Visibility.Collapsed;
+        }
+
+        private void ResetZoom(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            ((ScrollViewer)flipView.Items.ElementAt(flipView.SelectedIndex)).ZoomToFactor(1);
+        }
+
+        private void NavigateToPic(object sender, PointerRoutedEventArgs e)
+        {
+            if (allowClickOnLocationBar)
+            {
+                currentIndex = locationBar.Children.IndexOf((UIElement)sender) + 1;
+            }
+        }
+
+        internal void ResetTimer()
+        {
+            timer.Stop();
+            locationBar.Visibility = Visibility.Visible;
+            timer.Start();
+        }
+
+        internal void ManageLocationBarSelected()
+        {
+            if (locationBar.Children.Count >= currentIndex && currentIndex > 0)
+            {
+                foreach (Image pic in locationBar.Children)
+                {
+                    pic.Opacity = transparency;
+                }
+                locationBar.Children.ElementAt(currentIndex - 1).Opacity = 1;
+            }
+        }
+
+        internal void ManageCyclicalNavigation()
+        {
+            if (currentIndex == 0)
+            {
+                currentIndex = flipView.Items.Count - 2;
+            }
+            else if (currentIndex == flipView.Items.Count - 1)
+            {
+                flipView.SelectedIndex = 1;
+            }
+            if (flipView.Items.Count > 3)
+            {
+                ((ScrollViewer)flipView.Items.ElementAt(flipView.Items.Count - 1)).ZoomToFactor(((ScrollViewer)flipView.Items.ElementAt(1)).ZoomFactor);
+                ((ScrollViewer)flipView.Items.ElementAt(flipView.Items.Count - 1)).InvalidateScrollInfo();
+                ((ScrollViewer)flipView.Items.ElementAt(0)).ZoomToFactor(((ScrollViewer)flipView.Items.ElementAt(flipView.Items.Count - 2)).ZoomFactor);
+                ((ScrollViewer)flipView.Items.ElementAt(0)).InvalidateScrollInfo();
+            }
+        }
+
+    }
+}
